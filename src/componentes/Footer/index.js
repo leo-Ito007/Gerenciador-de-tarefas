8842f875c54de1447c0a6a0b7dc36facfd360df7
leo-Ito007 <leonardo.salles.felipe@escola.pr.gov.br>
2026-07-28 12:27:27 -0300

    Add files via upload

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,16 @@
+import './index.css';
+
+function Footer() {
+    return (
+        <footer>
+        <div className="colegio">
+            <p className="nomeColegio">Colegio Santa Maria Goretti -2026</p>
+        </div>
+        <div className="desenvolvedor">
+            <p className="nomeDesenvolvedor">Desenvolvido por Leonardo Ito Salles Flipe -2026</p>
+        </div>
+        </footer>
+    );
+    }
+
+    export default Footer;
